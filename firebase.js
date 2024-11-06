b34d654059f58fc22ffa73da5f3769c479d988f8
Prabhat <prabhatkumar.roy@outlook.com>
2024-11-07 01:25:03 +0530

    first commit

--- firebase.js
+++ firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxcJPzA_M9gH6PvkbOyEHXSGmPCG7GiZM",
-  authDomain: "uber-clone-a66fe.firebaseapp.com",
-  projectId: "uber-clone-a66fe",
-  storageBucket: "uber-clone-a66fe.appspot.com",
-  messagingSenderId: "25663664732",
-  appId: "1:25663664732:web:499fb47e45009a06fd6c99",
-  measurementId: "G-QJMPNW8HKR"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
